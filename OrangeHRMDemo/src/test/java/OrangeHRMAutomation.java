85d693ae61d3426d254e560329e9545fc7a633dd
unknown <bhavisachanandani@gmail.com>
2022-07-11 09:59:24 +0530

    Added new functionality

--- OrangeHRMDemo/src/test/java/OrangeHRMAutomation.java
+++ OrangeHRMDemo/src/test/java/OrangeHRMAutomation.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -22,6 +24,28 @@ public class OrangeHRMAutomation {
 		
 		//Step to maximize browser window
 		driver.manage().window().maximize();
+		
+		//Finding username textbox and entering value
+		WebElement userName=driver.findElement(By.id("txtUsername"));
+		userName.sendKeys("Admin");
+		
+		//Finding password textbox and entering value
+		WebElement password=driver.findElement(By.cssSelector("input#txtPassword"));
+		password.sendKeys("admin123");
+		
+		//Finding signIn and clicking
+		WebElement signIn=driver.findElement(By.xpath("//input[@value='LOGIN']"));
+		signIn.click();
+		
+		//Finding and displaying homepage user
+		String userNameText=driver.findElement(By.id("welcome")).getText();
+		System.out.println("User on home page is:"+userNameText);
+		
+		//Finding Assign Leave if it is displayed or not
+		boolean isAssignLeavePresent=driver.findElement(By.xpath("//span[text()='Assign Leave']")).isDisplayed();
+		System.out.println("is Assign Leave Check is:"+isAssignLeavePresent);
+		
+		Thread.sleep(3000);
 		
 		//Step to close browser
 		driver.quit();
